6cc6a8004dfc7b26b4671e8bc1aa137a141d0a5b
Kris Heinrich <krisheinrich@gmail.com>
2017-04-20 12:42:20 -0700

    add mLab URI

--- server/server.js
+++ server/server.js
@@ -7,25 +7,25 @@ const passport = require('passport');
 const passportConfig = require('./services/auth');
 const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
+const mongo = require('./credentials');
 
-// Create a new Express application
+// Create a new express instance
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://'+mongo.username+':'+mongo.password+'@ds163940.mlab.com:63940/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
-// Connect to the mongoDB instance and log a message
-// on success or failure
+// Connect to the mongoDB instance and log a message on success or failure
 mongoose.connect(MONGO_URI);
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
-// Configures express to use sessions.  This places an encrypted identifier
-// on the users cookie.  When a user makes a request, this middleware examines
+// Configures Express to use sessions. This places an encrypted identifier
+// on the users cookie. When a user makes a request, this middleware examines
 // the cookie and modifies the request object to indicate which user made the request
 // The cookie itself only contains the id of a session; more data about the session
 // is stored inside of MongoDB.
@@ -39,9 +39,9 @@ app.use(session({
   })
 }));
 
-// Passport is wired into express as a middleware. When a request comes in,
+// Passport is wired into Express as a middleware. When a request comes in,
 // Passport will examine the request's session (as set by the above config) and
-// assign the current user to the 'req.user' object.  See also servces/auth.js
+// assign the current user to the 'req.user' object. See also servces/auth.js
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -52,7 +52,7 @@ app.use('/graphql', expressGraphQL({
   graphiql: true
 }));
 
-// Webpack runs as a middleware.  If any request comes in for the root route ('/')
+// Webpack runs as a middleware. If any request comes in for the root route ('/')
 // Webpack will respond with the output of the webpack process: an HTML file and
 // a single bundle.js output of all of our client side Javascript
 const webpackMiddleware = require('webpack-dev-middleware');
